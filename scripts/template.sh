f889815367fc1af5c315faf7d3e8826273c89cd8
t-koba-96 <takukoba1996@gmail.com>
2020-08-27 16:40:36 +0000

    add

---
+++ scripts/template.sh
@@ -0,0 +1,9 @@
+python main.py template BEST apply_eyeliner
+
+python main.py template BEST braid_hair
+
+python main.py template BEST origami
+
+python main.py template BEST scrambled_eggs
+
+python main.py template BEST tie_tie
